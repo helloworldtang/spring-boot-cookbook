a1f9db2c1e934fc9b05782f1425e9ccd11acdefe
helloworldtang <helloworld.tang@qq.com>
2016-08-08 01:14:52 +0800

    增加日志打印 部署到tomcat

---
+++ src/main/java/com/web/Ch623Application.java
@@ -0,0 +1,38 @@
+package com.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.web.config.AuthorSettings;
+
+@RestController
+@SpringBootApplication
+public class Ch623Application extends SpringBootServletInitializer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Ch623Application.class);
+    @Autowired
+    private AuthorSettings authorSettings; //1
+
+    @RequestMapping("/")
+    public String index() {
+        String result = "author name is " + authorSettings.getName() + " and author age is " + authorSettings.getAge();
+        LOGGER.info("Visit / :{}", result);
+        return result;
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Ch623Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch623Application.class, args);
+    }
+}
