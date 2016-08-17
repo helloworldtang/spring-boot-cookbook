152ed26054aa64cffd047fed0c6b076f2806f9f3
tang.cheng <tang.cheng@xiaoyi.com>
2016-08-17 15:28:21 +0800

    fix bugs. add eventbus

--- src/main/java/com/Ch623Application.java
+++ src/main/java/com/Ch623Application.java
@@ -1,4 +1,4 @@
-package com.web;
+package com;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,7 +10,7 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.web.config.AuthorSettings;
+import com.config.AuthorSettings;
 
 @RestController
 @SpringBootApplication
