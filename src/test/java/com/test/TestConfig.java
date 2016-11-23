a6c734b1a341f017f492b80f63b4a7e28ec09dc1
tang.cheng <tang.cheng@xiaoyi.com>
2016-11-23 19:02:52 +0800

    Spring Autowired

---
+++ src/test/java/com/test/TestConfig.java
@@ -0,0 +1,12 @@
+package com.test;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by tang.cheng on 2016/11/23.
+ */
+@Configuration
+@ComponentScan("com.test")
+public class TestConfig {
+}
