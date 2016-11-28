62f47b3800be17d873bb01562abb5e360146c8cd
helloworldtang <helloworld.tang@qq.com>
2016-11-28 22:52:29 +0800

    @ConfigurationProperties  中使用@Valid

--- src/test/java/com/custom/ThreadSettingsTest.java
+++ src/test/java/com/custom/ThreadSettingsTest.java
@@ -5,8 +5,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.validation.BindException;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by MyWorld on 2016/8/12.
@@ -17,10 +18,19 @@ public class ThreadSettingsTest {
 
     @Autowired
     private ThreadSettings threadSettings;
-    @Test
+
+    /**
+     * Caused by: org.springframework.validation.BindException: org.springframework.validation.BeanPropertyBindingResult: 1 errors
+     * Field error in object 'thread' on field 'count': rejected value [1000]; codes [Max.thread.count,Max.count,Max.int,Max]; arguments [org.springframework.context.support.DefaultMessageSourceResolvable: codes [thread.count,count]; arguments []; default message [count],999]; default message [最大不能超过999]
+     * at org.springframework.boot.bind.PropertiesConfigurationFactory.validate(PropertiesConfigurationFactory.java:296)
+     * at org.springframework.boot.bind.PropertiesConfigurationFactory.doBindPropertiesToTarget(PropertiesConfigurationFactory.java:255)
+     * at org.springframework.boot.bind.PropertiesConfigurationFactory.bindPropertiesToTarget(PropertiesConfigurationFactory.java:227)
+     * at org.springframework.boot.context.properties.ConfigurationPropertiesBindingPostProcessor.postProcessBeforeInitialization(ConfigurationPropertiesBindingPostProcessor.java:296)
+     */
+    @Test(expected = BindException.class)
     public void initFromProperty() {
         int actual = threadSettings.getCount();
         int expected = 1000;
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
 }
