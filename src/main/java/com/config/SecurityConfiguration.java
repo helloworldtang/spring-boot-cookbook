fe742919f1c84cd295532bfd2105f93b4f86c11e
tang.cheng <tang.cheng@xiaoyi.com>
2017-01-05 14:11:04 +0800

    flyway not need auth

--- src/main/java/com/config/SecurityConfiguration.java
+++ src/main/java/com/config/SecurityConfiguration.java
@@ -37,7 +37,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/css/**", "/index").permitAll()
                 .antMatchers("/admin/**").hasRole("ADMIN")//Any URL that starts with "/admin/" will be restricted to users who have the role "ROLE_ADMIN". You will notice that since we are invoking the hasRole method we do not need to specify the "ROLE_" prefix.
                 .antMatchers("/db/**").access("hasRole('ROLE_ADMIN') and hasRole('ROLE_DBA')")
-                .antMatchers("/flyway").hasRole("ADMIN")
+                .antMatchers("/flyway").permitAll()
                 .anyRequest().authenticated()//Any URL that has not already been matched on only requires that the user be authenticated
                 .and()
                 .formLogin().loginPage("/login")
