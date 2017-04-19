4012f8f943374f55e2e8e3243d4243ef7e542008
tang.cheng <tang.cheng@xiaoyi.com>
2017-04-19 20:42:33 +0800

    优化提示信息

--- src/main/java/com/tangcheng/global/exception/GlobalExHandler.java
+++ src/main/java/com/tangcheng/global/exception/GlobalExHandler.java
@@ -3,13 +3,22 @@ package com.tangcheng.global.exception;
 import com.tangcheng.global.domain.ResultData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLException;
 
 /**
  * Created by MyWorld on 2016/9/23.
  */
 @RestControllerAdvice
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class GlobalExHandler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExHandler.class);
@@ -27,6 +36,19 @@ public class GlobalExHandler {
     }
 
 
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity handleBindException(BindException e) {
+        LOGGER.error(e.getMessage(), e);
+        StringBuilder result = new StringBuilder();
+        for (FieldError fieldError : e.getFieldErrors()) {
+            result.append(fieldError.getField()).append(":").
+                    append(fieldError.getDefaultMessage()).
+                    append(System.getProperty("line.separator"));
+        }
+        return ResponseEntity.badRequest().body(result.toString());
+    }
+
+
     /**
      * 处理未知异常
      *
@@ -36,6 +58,9 @@ public class GlobalExHandler {
     @ExceptionHandler(Exception.class)
     public ResultData<String> throwable(Exception e) {
         LOGGER.error(e.getMessage(), e);
+        if (e instanceof SQLException || e instanceof DataAccessException) {
+            return new ResultData<>(GlobalCode.FAIL, "服务器好像开小差了，等会再试下:-) ");
+        }
         return new ResultData<>(GlobalCode.FAIL, e.getMessage());
     }
 
