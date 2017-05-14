ec474b06672be440b603fbc84d309d49af91b284
Tangcheng <helloworld.tang@qq.com>
2017-05-14 23:37:31 +0800

    authorization server

---
+++ app-authorization-server/src/main/java/com/tangcheng/app/authorization/AuthorizationApplication.java
@@ -0,0 +1,11 @@
+package com.tangcheng.app.authorization;
+
+/**
+ * Created by tangcheng on 5/14/2017.
+ */
+//@SpringBootApplication
+public class AuthorizationApplication {
+    public static void main(String[] args) {
+
+    }
+}
