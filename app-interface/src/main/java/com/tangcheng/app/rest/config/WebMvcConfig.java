29286b7da68778bb76b5d7887887873a5b0f2b8d
Tangcheng <helloworld.tang@qq.com>
2017-07-10 22:19:41 +0800

    TrailingSlashMatch SuffixPatternMatch

--- app-interface/src/main/java/com/tangcheng/app/rest/config/WebMvcConfig.java
+++ app-interface/src/main/java/com/tangcheng/app/rest/config/WebMvcConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
@@ -28,6 +29,27 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+//        configurePathMatch(PathMatchConfigurer configurer)函数让开发人员可以根据需求定制URL路径的匹配规则。
+//        configurer.setUseSuffixPatternMatch(false)表示设计人员希望系统对外暴露的URL不会识别和匹配.*后缀。
+// 在这个例子中，就意味着Spring会将793059909@qq.com当做一个{email}参数传给Controller层接口
+//        configurer.setUseTrailingSlashMatch(true)表示系统不区分URL的最后一个字符是否是斜杠/。
+// 在这个例子中，就意味着http://localhost:8080/email/793059909@qq.com和http://localhost:8080/email/793059909@qq.com/含义相同
+
+//        后缀模式匹配模式：
+        //如果没有setUseSuffixPatternMatch(false)的设置，则http://localhost/hello和http://localhost/hello.html会访问相同的接口，
+        //因为/hello是可以匹配到/hello.*的，/hello.test,/hello.world都可以访问到/hello接口
+
+//        末尾斜杠匹配
+//        setUseTrailingSlashMatch (boolean useSuffixPatternMatch)：
+//        设置是否自动后缀路径模式匹配，如“/user”是否匹配“/user/”，默认真即匹配；
+//        当此参数设置为true的会后，那么地址/user，/user/都能正常访问。
+//        当此参数设置为false的时候，那么就只能访问/user了
+        configurer.setUseSuffixPatternMatch(false)
+                .setUseTrailingSlashMatch(true);
     }
 
     /**
