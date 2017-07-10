74a1876954c9216196ea69bbca3f673bc97565c1
tang.cheng <tang.cheng@xiaoyi.com>
2017-07-10 19:55:33 +0800

    messageformat number会被格式化

--- learning-demo/src/test/java/com/tangcheng/learning/oo/MessageFormatTest.java
+++ learning-demo/src/test/java/com/tangcheng/learning/oo/MessageFormatTest.java
@@ -26,6 +26,11 @@ public class MessageFormatTest {
                 "At {1,time} on {1,date}, there was {2} on planet {0,number,integer}.",
                 arguments);
         System.out.println(result);
+        System.out.println("Long和Date类型的数据，在输出时会被格式化。保守的做法，就是把参数都转换成String");
+        Integer num = 1234567890;
+        String longStyle = MessageFormat.format("expect:1234567890.but:{0}.str:{1}", num, num.toString());
+//        expect:1234567890.but:1,234,567,890.str:1234567890
+        System.out.println(longStyle);
     }
 
 }
