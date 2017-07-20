ba9427e2faef9ff78199584acd528c18ea4d69bc
tang.cheng <tang.cheng@xiaoyi.com>
2017-07-20 18:55:21 +0800

    optimize

--- app-interface/src/main/java/com/tangcheng/app/rest/controller/CityController.java
+++ app-interface/src/main/java/com/tangcheng/app/rest/controller/CityController.java
@@ -3,10 +3,13 @@ package com.tangcheng.app.rest.controller;
 import com.tangcheng.app.core.aop.MethodLogAnnotation;
 import com.tangcheng.app.domain.entity.CityDo;
 import com.tangcheng.app.service.biz.CityService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
@@ -15,12 +18,15 @@ import java.util.List;
 @RestController
 public class CityController {
 
+    public static final Logger LOGGER = LoggerFactory.getLogger(CityController.class);
+
     @Autowired
     private CityService cityService;
 
     @MethodLogAnnotation(desc = "查找City")
     @RequestMapping(value = "/city", method = RequestMethod.GET)
-    public ResponseEntity<List<CityDo>> getCity(@RequestParam("state") String state) {
+    public ResponseEntity<List<CityDo>> getCity(@RequestParam("state") String state, HttpServletRequest request) {
+        LOGGER.info("{},{},{}", request.getRemoteAddr(), request.getRemoteHost(), request.getRemotePort());
         return ResponseEntity.ok(cityService.getCity(state));
     }
 
