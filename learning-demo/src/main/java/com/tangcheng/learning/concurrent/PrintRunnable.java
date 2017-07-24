1acc5201bc3c47616377fb136840db3e331c7836
tang.cheng <tang.cheng@xiaoyi.com>
2017-07-24 19:53:48 +0800

    fix bugs: service.shutdown()  not valid for the thread not be over

--- learning-demo/src/main/java/com/tangcheng/learning/concurrent/PrintRunnable.java
+++ learning-demo/src/main/java/com/tangcheng/learning/concurrent/PrintRunnable.java
@@ -1,5 +1,8 @@
 package com.tangcheng.learning.concurrent;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -8,11 +11,14 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class PrintRunnable implements Runnable {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(PrintRunnable.class);
+
     private Lock lock;
     private Condition condition;
     private String symbol;
     private int flag = 0;
-    private static AtomicInteger i = new AtomicInteger(0);
+    private static AtomicInteger globalFlag = new AtomicInteger(0);
 
     public PrintRunnable(Lock lock, Condition condition, String symbol, int flag) {
         this.lock = lock;
@@ -31,7 +37,6 @@ public class PrintRunnable implements Runnable {
             service.execute(new PrintRunnable(lock, condition, signal[i], i));
         }
         service.shutdown();
-        //todo 目前这种写法有两个线程没有结束，导致无法shutdown
     }
 
     @Override
@@ -39,20 +44,27 @@ public class PrintRunnable implements Runnable {
         while (true) {
             lock.lock();
             try {
-                while (i.get() % 3 != flag) {
+                while (globalFlag.get() % 3 != flag) {
                     condition.await();
                 }
+                LOGGER.info("{}->result:{}", globalFlag.get(), symbol);
+                condition.signalAll();
+                //计划输出3个周期
+                // flag为0的会在6(3+3+flag)时结束，结束前会signalALl,
+                // flag为1的会在7(3+3+flag)时结束，结束前会signalALl,
+                // flag为2的会在8(3+3+flag)时结束，结束前signalAll(lock相关的对象都操作完了)
+                int exitFlag = 3 * 2 + flag;
+                int current = globalFlag.get();
+                globalFlag.getAndIncrement();
+                if (current == exitFlag) break;
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                LOGGER.error(e.getMessage(), e);
+            } finally {
+                lock.unlock();
             }
-            //防止过多输出  
-            if (i.get() > 8) break;
-            System.out.println(Thread.currentThread() + "->" + i.get() + "result " + symbol);
-            i.getAndIncrement();
-            condition.signalAll();
-            lock.unlock();
+
         }
-        System.out.println(Thread.currentThread() + "->end");
+        LOGGER.info("end");
 
     }
 }  
