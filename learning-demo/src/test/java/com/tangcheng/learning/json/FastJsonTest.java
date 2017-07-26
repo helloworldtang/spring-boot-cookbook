6247169f955c5784c1cefd3cccb21f875a2ca3fd
tang.cheng <tang.cheng@xiaoyi.com>
2017-07-26 18:12:17 +0800

    fastjson jsonpath generics

--- learning-demo/src/test/java/com/tangcheng/learning/json/FastJsonTest.java
+++ learning-demo/src/test/java/com/tangcheng/learning/json/FastJsonTest.java
@@ -1,9 +1,17 @@
 package com.tangcheng.learning.json;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.JSONPath;
+import com.alibaba.fastjson.TypeReference;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -28,5 +36,43 @@ public class FastJsonTest {
         assertThat("is string with 'null'", "null", is(jsonString));
 
     }
+
+
+    @Test
+    public void testJsonPath() throws IOException {
+        InputStream resourceAsStream = this.getClass().getResourceAsStream("/JsonPathTestData.json");
+        String jsonStr = IOUtils.toString(resourceAsStream, "utf-8");
+        System.out.println(jsonStr);
+
+        System.out.println(jsonStr);
+        JSONObject jsonObject = JSON.parseObject(jsonStr);
+
+        System.out.println("\nBook数目：" + JSONPath.eval(jsonObject, "$.store.book.size()"));
+        System.out.println("第一本书title：" + JSONPath.eval(jsonObject, "$.store.book[0].title"));
+        System.out.println("price大于10元的book：" + JSONPath.eval(jsonObject, "$.store.book[price > 10]"));
+        System.out.println("price大于10元的title：" + JSONPath.eval(jsonObject, "$.store.book[price > 10][0].title"));
+        System.out.println("category(类别)为fiction(小说)的book：" + JSONPath.eval(jsonObject, "$.store.book[category = 'fiction']"));
+        System.out.println("bicycle的所有属性值" + JSONPath.eval(jsonObject, "$.store.bicycle.*"));
+        System.out.println("bicycle的color和price属性值" + JSONPath.eval(jsonObject, "$.store.bicycle['color','price']"));
+    }
+
+
+    @Test
+    public void testGenerics() {
+        Result<User> result = new Result<>();
+        result.setMsg("Success");
+        List<User> users = new ArrayList<>();
+        users.add(new User(1L, "Name1"));
+        users.add(new User(2L, "Name2"));
+        result.setModule(users);
+        String js = JSON.toJSONString(result);
+        System.out.println(js);
+        //java.lang.ClassCastException: com.alibaba.fastjson.JSONObject cannot be cast to com.tangcheng.learning.json.User
+//        Result<User> obj = (Result<User>) JSON.parseObject(js, Result.class);
+        Result<User> userResult = JSON.parseObject(js, new TypeReference<Result<User>>() {
+        });
+        System.out.println(userResult);
+    }
+
 
 }
