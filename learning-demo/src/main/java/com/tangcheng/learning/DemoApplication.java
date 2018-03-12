ee610e2cf822c7f63a1e0cc250cfd8f1a0e5850b
唐成 <tangcheng@hujiang.com>
2018-03-12 11:59:15 +0800

    swagger,Spring MVC会自动将long型的时间转换成java.util.Date,logging.file

--- learning-demo/src/main/java/com/tangcheng/learning/DemoApplication.java
+++ learning-demo/src/main/java/com/tangcheng/learning/DemoApplication.java
@@ -1,11 +1,13 @@
 package com.tangcheng.learning;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Created by tang.cheng on 2017/4/22.
  */
+@EnableSwagger2Doc
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
---
+++ learning-demo/src/main/java/com/tangcheng/learning/web/controller/TestController.java
@@ -0,0 +1,38 @@
+package com.tangcheng.learning.web.controller;
+
+import com.tangcheng.learning.web.dto.req.DateReq;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.text.SimpleDateFormat;
+
+/**
+ * @author tangcheng
+ * 2018/03/12
+ */
+@Api(tags = "TestCase", description = "MVC相关功能TestCase")
+@RestController
+@Slf4j
+public class TestController {
+
+    /**
+     * Spring MVC会自动将long型的时间转换成java.util.Date
+     *
+     * @param dateReq 请求信息
+     * @return
+     */
+    @ApiOperation("Spring MVC会自动将long型的时间转换成java.util.Date")
+    @PostMapping("/test/book-time")
+    public ResponseEntity<Object> postBookTime(@Valid @RequestBody DateReq dateReq) {
+        String formatDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(dateReq.getBookTime());
+        log.info("{},{}", formatDate, dateReq.getBookTime());
+        return ResponseEntity.ok().body(formatDate);
+    }
+
+}
