1971b11e96a7fbec3fdc80758631f9fd012eba96
唐成 <tangcheng@hujiang.com>
2018-04-09 11:27:35 +0800

    add java8 stream map collect ut

--- learning-demo/src/test/java/com/tangcheng/learning/java8/StreamTest.java
+++ learning-demo/src/test/java/com/tangcheng/learning/java8/StreamTest.java
@@ -1,14 +1,17 @@
 package com.tangcheng.learning.java8;
 
+import com.tangcheng.learning.json.User;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
+import static org.assertj.core.api.Assertions.assertThat;
 
+@Slf4j
 public class StreamTest {
 
     @Test
@@ -16,7 +19,24 @@ public class StreamTest {
         String expect = "1,2,3,4,5";
         List<String> source = Arrays.asList("1", "2", "3", "4", "5");
         String actual = source.stream().collect(Collectors.joining(","));
-        assertThat(actual, is(expect));
+        assertThat(actual).isEqualTo(expect);
+    }
+
+
+    @Test
+    public void joinListWithPO() {
+        List<User> circleList = new ArrayList<User>() {
+            {
+                add(new User(1L, "1"));
+                add(new User(2L, "2"));
+                add(new User(5L, "5"));
+                add(new User(3L, "3"));
+            }
+        };
+        String expect = "1,2,5,3";
+        String actual = circleList.stream().map(User::getName).collect(Collectors.joining(","));
+        log.info("actual:{}", actual);
+        assertThat(actual).isEqualTo(expect);
     }
 
 }
