e2b347be756454a3ff0eab17815e813579d64867
唐成 <tangcheng@hujiang.com>
2018-04-12 13:30:40 +0800

    add comment

--- learning-demo/src/test/java/com/tangcheng/learning/joda/JodaTest.java
+++ learning-demo/src/test/java/com/tangcheng/learning/joda/JodaTest.java
@@ -11,11 +11,17 @@ import org.junit.Test;
 @Slf4j
 public class JodaTest {
 
+    /**
+     * 13:28:51.527 [main] INFO com.tangcheng.learning.joda.JodaTest - end:2018-04-13 00:00:00 000
+     * 13:28:51.531 [main] INFO com.tangcheng.learning.joda.JodaTest - now:2018-04-12 13:28:51 051
+     * 13:28:51.532 [main] INFO com.tangcheng.learning.joda.JodaTest - origin:hours:10,minutes:631,seconds:37868
+     * 13:28:51.541 [main] INFO com.tangcheng.learning.joda.JodaTest - hours:10,minutes:631,seconds:37868
+     */
     @Test
     public void duringHoursMinutes() {
         LocalDateTime end = new LocalDateTime(LocalDate.now().plusDays(1).toDate());
         LocalDateTime now = LocalDateTime.now();
-        String pattern = "yyyy-MM-dd HH:mm:ss ssss";
+        String pattern = "yyyy-MM-dd HH:mm:ss sss";
         log.info("end:{}", end.toString(pattern));
         log.info("now:{}", now.toString(pattern));
         long secondsOrigin = (end.toDate().getTime() - now.toDate().getTime()) / 1000;
