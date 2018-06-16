65e4248a07492eeb66f6d45561403fbd0df61bc8
Tangcheng <helloworld.tang@qq.com>
2018-06-17 03:35:02 +0800

    分布式锁的一个ut

---
+++ learning-demo/src/main/java/com/tangcheng/learning/service/DistributeLockService.java
@@ -0,0 +1,20 @@
+package com.tangcheng.learning.service;
+
+import com.tangcheng.learning.service.lock.annotation.DistributedLock;
+import com.tangcheng.learning.service.lock.annotation.KeyParam;
+import com.tangcheng.learning.web.dto.req.DistributeLockTestReq;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * spring-boot-cookbook
+ *
+ * @author tangcheng
+ * @date 6/17/2018 2:24 AM
+ */
+public interface DistributeLockService {
+
+
+    @DistributedLock
+    void mayBeMultiRepeatRequest(@KeyParam(name = "id") @RequestParam Long id, DistributeLockTestReq req);
+}
