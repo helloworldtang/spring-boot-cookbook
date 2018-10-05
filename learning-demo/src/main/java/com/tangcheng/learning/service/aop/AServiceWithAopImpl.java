6cd815e5228f68b1783918f875174b0a34b70de4
tangcheng <tangcheng@hujiang.com>
2018-10-05 14:19:53 +0800

    请注意：如果Spring上下文中的Bean添加了JDK动态代理特性，那么代理是不会织入到具体的实现类Bean的，即使用接口的具体实现类进行注入时，代理将失效（虽然实现类也实现了相同的接口）。 http://www.spring4all.com/article/1588

---
+++ learning-demo/src/main/java/com/tangcheng/learning/service/aop/AServiceWithAopImpl.java
@@ -0,0 +1,24 @@
+package com.tangcheng.learning.service.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * spring-boot-cookbook
+ *
+ * @author tangcheng
+ * @date 10/5/2018 12:55 PM
+ */
+@Service
+@Slf4j
+@Transactional
+public class AServiceWithAopImpl implements AServiceWithAop {
+
+    @Transactional
+    @Override
+    public void doBiz() {
+        log.info("a finished");
+    }
+
+}
---
+++ learning-demo/src/main/java/com/tangcheng/learning/service/aop/BServiceWithAopImpl.java
@@ -0,0 +1,29 @@
+package com.tangcheng.learning.service.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * spring-boot-cookbook
+ *
+ * @author tangcheng
+ * @date 10/5/2018 12:57 PM
+ */
+@Service
+@Slf4j
+@Transactional
+public class BServiceWithAopImpl implements BServiceWithAop {
+
+    @Autowired
+    private AServiceWithAopImpl aServiceWithAop;
+
+    @Override
+    public void doBiz() {
+        aServiceWithAop.doBiz();
+        log.info("b finished");
+    }
+
+
+}
