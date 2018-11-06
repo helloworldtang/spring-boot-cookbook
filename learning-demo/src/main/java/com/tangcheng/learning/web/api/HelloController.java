9b461f744af06f004b1ec3c45523a1a66554b4a6
tangcheng <tangcheng@hujiang.com>
2018-11-06 21:39:51 +0800

    API中HttpServletRequest的使用

--- learning-demo/src/main/java/com/tangcheng/learning/web/api/HelloController.java
+++ learning-demo/src/main/java/com/tangcheng/learning/web/api/HelloController.java
@@ -1,15 +1,26 @@
 package com.tangcheng.learning.web.api;
 
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
+@Api(tags = "UT,IT示例类", description = "UT,IT示例类")
+@Slf4j
 @RestController
 public class HelloController {
 
     public static final String GREETINGS_FROM_SPRING_BOOT = "Greetings from Spring Boot!";
 
-    @RequestMapping("/v1/hello")
-    public String index() {
+    @GetMapping("/v1/hello")
+    public String index(HttpServletRequest httpServletRequest) {
+        log.info("ip:{} 客户机的端口:{} 服务端口:{}  请求最终在哪个端口结束:{}", httpServletRequest.getRemoteAddr(),
+                httpServletRequest.getRemotePort(),
+                httpServletRequest.getServerPort(),
+                httpServletRequest.getLocalPort()
+        );
         return GREETINGS_FROM_SPRING_BOOT;
     }
 
