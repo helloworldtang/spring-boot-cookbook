e74e9a55c4cc9e25d4c9a4561b231b68cba2c49f
唐成 <cheng.tang@gongbangbang.com>
2019-03-14 17:52:40 +0800

    random string generator

--- learning-demo/src/test/java/com/tangcheng/learning/string/RandomStringGeneratorTest.java
+++ learning-demo/src/test/java/com/tangcheng/learning/string/RandomStringGeneratorTest.java
@@ -1,7 +1,9 @@
 package com.tangcheng.learning.string;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.text.RandomStringGenerator;
+import org.apache.commons.text.TextRandomProvider;
 import org.junit.Test;
 
 /**
@@ -14,6 +16,39 @@ public class RandomStringGeneratorTest {
 
     /**
      * https://commons.apache.org/proper/commons-text/javadocs/api-release/org/apache/commons/text/RandomStringGenerator.html
+     */
+
+    /**
+     * <p>
+     * Generates random Unicode strings containing the specified number of code points.
+     * Instances are created using a builder class, which allows the
+     * callers to define the properties of the generator. See the documentation for the
+     * {@link RandomStringGenerator.Builder} class to see available properties.
+     * </p>
+     * <pre>
+     * // Generates a 20 code point string, using only the letters a-z
+     * RandomStringGenerator generator = new RandomStringGenerator.Builder()
+     *     .withinRange('a', 'z').build();
+     * String randomLetters = generator.generate(20);
+     * </pre>
+     * <pre>
+     * // Using Apache Commons RNG for randomness
+     * UniformRandomProvider rng = RandomSource.create(...);
+     * // Generates a 20 code point string, using only the letters a-z
+     * RandomStringGenerator generator = new RandomStringGenerator.Builder()
+     *     .withinRange('a', 'z')
+     *     .usingRandom(rng::nextInt) // uses Java 8 syntax
+     *     .build();
+     * String randomLetters = generator.generate(20);
+     * </pre>
+     * <p>
+     * {@code RandomStringBuilder} instances are thread-safe when using the
+     * default random number generator (RNG). If a custom RNG is set by calling the method
+     * {@link RandomStringGenerator.Builder#usingRandom(TextRandomProvider) Builder.usingRandom(TextRandomProvider)}, thread-safety
+     * must be ensured externally.
+     * </p>
+     *
+     * @since 1.1
      */
     @Test
     public void generateRandomStringDemo() {
@@ -38,5 +73,31 @@ public class RandomStringGeneratorTest {
         System.out.println(StringUtils.center("随机混合字符串", 20, "="));
         System.out.println(random);
     }
+
+
+    /**
+     * <p>Operations for random {@code String}s.</p>
+     * <p>Currently <em>private high surrogate</em> characters are ignored.
+     * These are Unicode characters that fall between the values 56192 (db80)
+     * and 56319 (dbff) as we don't know how to handle them.
+     * High and low surrogates are correctly dealt with - that is if a
+     * high surrogate is randomly chosen, 55296 (d800) to 56191 (db7f)
+     * then it is followed by a low surrogate. If a low surrogate is chosen,
+     * 56320 (dc00) to 57343 (dfff) then it is placed after a randomly
+     * chosen high surrogate. </p>
+     *
+     * <p>#ThreadSafe#</p>
+     *
+     * @since 1.0
+     * @deprecated as of 3.6, use commons-text
+     * <a href="https://commons.apache.org/proper/commons-text/javadocs/api-release/org/apache/commons/text/RandomStringGenerator.html">
+     * RandomStringGenerator</a> instead
+     */
+    @Test
+    public void test() {
+        String random = RandomStringUtils.random(5, true, true);
+        System.out.println(random);
+    }
+
 
 }
