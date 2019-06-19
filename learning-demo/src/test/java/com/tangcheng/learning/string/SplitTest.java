c955c68ec82426c0620b8fc51f9a9cf10186788c
唐成 <cheng.tang@gongbangbang.com>
2019-06-19 14:00:32 +0800

    Arrays.asList()

--- learning-demo/src/test/java/com/tangcheng/learning/string/SplitTest.java
+++ learning-demo/src/test/java/com/tangcheng/learning/string/SplitTest.java
@@ -19,6 +19,15 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class SplitTest {
 
+    /**
+     * 使用工具类 Arrays.asList()把数组转换成集合时，不能使用其修改集合相关的方法，它的
+     * add/remove/clear 方法会抛出 UnsupportedOperationException 异常。
+     * 说明：asList 的返回对象是一个 Arrays 内部类，并没有实现集合的修改方法。Arrays.asList 体现的
+     * 是适配器模式，只是转换接口，后台的数据仍是数组。
+     * String[] str = new String[] { "you", "wu" }; List list = Arrays.asList(str);
+     * 第一种情况：list.add("yangguanbao"); 运行时异常。
+     * 第二种情况：str[0] = "gujin"; 那么 list.get(0)也会随之修改。
+     */
     @Test
     public void splitLengthTest() {
         String str = "a,b,c,,";
