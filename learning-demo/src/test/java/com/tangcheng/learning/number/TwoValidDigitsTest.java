fa3939bc061d9909d5bdfc72721388feee86b8fb
唐成 <cheng.tang@gongbangbang.com>
2019-07-11 14:21:33 +0800

    BigDecimal四舍五入

--- learning-demo/src/test/java/com/tangcheng/learning/number/TwoValidDigitsTest.java
+++ learning-demo/src/test/java/com/tangcheng/learning/number/TwoValidDigitsTest.java
@@ -64,6 +64,11 @@ public class TwoValidDigitsTest {
 
     @Test
     public void m1() {
+        /**
+         * 向“最接近的”数字舍入，如果与两个相邻数字的距离相等，则为向上舍入的舍入模式。
+         * 如果舍弃部分 >= 0.5，则舍入行为与 ROUND_UP 相同;否则舍入行为与 ROUND_DOWN 相同。
+         * 注意，这是我们大多数人在小学时就学过的舍入模式(四舍五入)。
+         */
         double sourceEgLessThan5 = 123232.12345;
         BigDecimal bd = new BigDecimal(sourceEgLessThan5);
         double actual = bd.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
@@ -73,6 +78,41 @@ public class TwoValidDigitsTest {
         bd = new BigDecimal(sourceEgLessThan5);
         actual = bd.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
         assertThat(actual).isEqualTo(123232.12);
+
+        sourceEgLessThan5 = 123232.12445;
+        bd = new BigDecimal(sourceEgLessThan5);
+        actual = bd.setScale(0, BigDecimal.ROUND_HALF_UP).doubleValue();
+        assertThat(actual).isEqualTo(123232);
+
+        sourceEgLessThan5 = 123232.52445;
+        bd = new BigDecimal(sourceEgLessThan5);
+        actual = bd.setScale(0, BigDecimal.ROUND_HALF_UP).doubleValue();
+        assertThat(actual).isEqualTo(123233);
+
+        /**
+         * 接近零的舍入模式。
+         * 在丢弃某部分之前始终不增加数字(从不对舍弃部分前面的数字加1，即截短)。
+         * 注意，此舍入模式始终不会增加计算值的大小。
+         */
+        sourceEgLessThan5 = 123232.52445;
+        bd = new BigDecimal(sourceEgLessThan5);
+        actual = bd.setScale(0, BigDecimal.ROUND_DOWN).doubleValue();
+        assertThat(actual).isEqualTo(123232);
+
+        /**
+         * 接近零的舍入模式。
+         * 在丢弃某部分之前始终不增加数字(从不对舍弃部分前面的数字加1，即截短)。
+         * 注意，此舍入模式始终不会增加计算值的大小。
+         */
+        sourceEgLessThan5 = 123232.52445;
+        bd = new BigDecimal(sourceEgLessThan5);
+        actual = bd.setScale(0, BigDecimal.ROUND_UP).doubleValue();
+        assertThat(actual).isEqualTo(123233);
+
+        sourceEgLessThan5 = 123232.12445;
+        bd = new BigDecimal(sourceEgLessThan5);
+        actual = bd.setScale(0, BigDecimal.ROUND_UP).doubleValue();
+        assertThat(actual).isEqualTo(123233);
 
         double sourceEgEqual5 = 123232.12545;
         bd = new BigDecimal(sourceEgEqual5);
