4fdac7e1a49bb6b15f491b20795198169d92cb31
唐成 <cheng.tang@gongbangbang.com>
2019-07-26 18:24:30 +0800

    使用一个接口同时具体Get和Post行为【提供给第三方回调时可能有用】

--- learning-demo/src/main/java/com/tangcheng/learning/web/api/HelloController.java
+++ learning-demo/src/main/java/com/tangcheng/learning/web/api/HelloController.java
@@ -1,8 +1,10 @@
 package com.tangcheng.learning.web.api;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,7 +16,8 @@ public class HelloController {
 
     public static final String GREETINGS_FROM_SPRING_BOOT = "Greetings from Spring Boot!";
 
-    @GetMapping("/v1/hello")
+    @ApiOperation("使用一个接口同时具体Get和Post行为【提供给第三方回调时可能有用】")
+    @RequestMapping(value = "/v1/hello", method = {RequestMethod.GET, RequestMethod.POST})
     public String index(HttpServletRequest request) {
         log.info("ip:{} 客户机的端口:{} 服务端口:{}  请求最终在哪个端口结束:{}", request.getRemoteAddr(),
                 request.getRemotePort(),
