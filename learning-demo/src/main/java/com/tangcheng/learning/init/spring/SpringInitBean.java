90dc38b61d6317c9fa574adc30a6720faa987d9b
唐成 <cheng.tang@gongbangbang.com>
2019-07-26 15:35:43 +0800

    BeanPostProcessor，针对所有Spring上下文中所有的bean 【 (or a BeanPostProcessor involved) returned null 】    fix bug: org.springframework.context.ApplicationContextException: Unable to start embedded container; nested exception is org.springframework.beans.factory.BeanCreationException: Error creating bean with name 'tomcatEmbeddedServletContainerFactory' defined in class path resource [org/springframework/boot/autoconfigure/web/EmbeddedServletContainerAutoConfiguration$EmbeddedTomcat.class]: factory-bean 'org.springframework.boot.autoconfigure.web.EmbeddedServletContainerAutoConfiguration$EmbeddedTomcat' (or a BeanPostProcessor involved) returned null

--- learning-demo/src/main/java/com/tangcheng/learning/init/spring/SpringInitBean.java
+++ learning-demo/src/main/java/com/tangcheng/learning/init/spring/SpringInitBean.java
@@ -2,7 +2,11 @@ package com.tangcheng.learning.init.spring;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.*;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -10,6 +14,14 @@ import org.springframework.context.ApplicationContextAware;
 import javax.annotation.PostConstruct;
 
 /**
+ * afterPropertiesSet 和init-method之间的执行顺序是
+ * afterPropertiesSet 先执行，init-method 后执行。
+ * 从BeanPostProcessor的作用，可以看出
+ * 最先执行的是postProcessBeforeInitialization，
+ * 然后是afterPropertiesSet，
+ * 然后是init-method，
+ * 然后是postProcessAfterInitialization
+ * <p>
  * Created by tangcheng on 2017/1/5.
  */
 @Slf4j
@@ -20,11 +32,11 @@ public class SpringInitBean implements BeanNameAware, BeanFactoryAware, Applicat
      * 2.Spring将值和bean的引用注入到bean对应的属性中
      * 3.如果bean实现了BeanNameAware接口，Spring将bean的ID传递给setBeanName()方法；
      *
-     * @param s
+     * @param name
      */
     @Override
-    public void setBeanName(String s) {
-        log.info("Step-3调用。BeanNameAware.setBeanName {}", s);
+    public void setBeanName(String name) {
+        log.info("Step-3调用。BeanNameAware.setBeanName {}", name);
     }
 
     @Override
@@ -38,11 +50,21 @@ public class SpringInitBean implements BeanNameAware, BeanFactoryAware, Applicat
         log.info("Step-5调用。ApplicationContextAware.setApplicationContext {}", applicationContext);
     }
 
-
+    /**
+     * BeanPostProcessor，针对所有Spring上下文中所有的bean，可以在配置文档 applicationContext.xml 中配置一个 BeanPostProcessor ，然后对所有的bean进行一个初始化之前和之后的代理。
+     * BeanPostProcessor 接口中有两个方法： postProcessBeforeInitialization 和 postProcessAfterInitialization 。
+     * postProcessBeforeInitialization 方法在bean初始化之前执行，
+     * postProcessAfterInitialization 方法在bean初始化之后执行。
+     *
+     * @param bean
+     * @param beanName
+     * @return
+     * @throws BeansException
+     */
     @Override
-    public Object postProcessBeforeInitialization(Object o, String s) throws BeansException {
-        log.info("Step-6调用。BeanPostProcessor.postProcessBeforeInitialization o={},s={}", o, s);
-        return null;
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        log.info("Step-6调用。BeanPostProcessor.postProcessBeforeInitialization bean={},beanName={}", bean.getClass().getCanonicalName(), beanName);
+        return bean;
     }
 
     @Override
@@ -52,9 +74,9 @@ public class SpringInitBean implements BeanNameAware, BeanFactoryAware, Applicat
     }
 
     @Override
-    public Object postProcessAfterInitialization(Object o, String s) throws BeansException {
-        log.info("Step-8调用。BeanPostProcessor.postProcessAfterInitialization o={},s={}", o, s);
-        return null;
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        log.info("Step-8调用。BeanPostProcessor.postProcessAfterInitialization bean={},beanName={}", bean, beanName);
+        return bean;
     }
 
     @Override
