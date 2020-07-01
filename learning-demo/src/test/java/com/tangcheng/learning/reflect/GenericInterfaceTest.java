98c6e6e654b1f9d2c58883a3e1d114853264b4b9
唐成 <cheng.tang@gongbangbang.com>
2020-07-01 11:08:55 +0800

    根据签名和方法名找方法；判断方法签名中有没有指定的方法

--- learning-demo/src/test/java/com/tangcheng/learning/reflect/GenericInterfaceTest.java
+++ learning-demo/src/test/java/com/tangcheng/learning/reflect/GenericInterfaceTest.java
@@ -7,7 +7,10 @@ import org.junit.Test;
 import org.springframework.util.ReflectionUtils;
 
 import java.lang.reflect.Method;
-import java.util.Collections;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -31,7 +34,6 @@ public class GenericInterfaceTest {
 
         Method notExistsMethod = ReflectionUtils.findMethod(eventHandler.getClass(), "notExists", Integer.class);
         assertThat(notExistsMethod).isNull();
-
     }
 
 
@@ -43,6 +45,65 @@ public class GenericInterfaceTest {
 
         Method methodParameterCanntChangeOrder = ReflectionUtils.findMethod(eventHandler.getClass(), "support", Integer.class, String.class);
         assertThat(methodParameterCanntChangeOrder).isNull();
+    }
+
+
+    @Test
+    public void checkMethodSignature() {
+        Integer integerSignature = 1;
+        EventHandler<Integer> eventHandler = new IntegerEventHandler();
+        Class<?> searchType = eventHandler.getClass();
+        while (searchType != null) {
+            Method[] methods = (searchType.isInterface() ? searchType.getMethods() : getDeclareMethods(searchType));
+            for (Method method : methods) {
+                if ("support".equals(method.getName())) {
+                    Class<?>[] parameterTypes = method.getParameterTypes();
+                    for (Class<?> parameterType : parameterTypes) {
+                        if (Objects.equals(parameterType, integerSignature.getClass())) {
+                            System.out.println(searchType.getName() + ":" + method.getName() + ":ParameterTypes.length:" + parameterTypes.length + ":success");
+                        }
+                    }
+                }
+            }
+            searchType = searchType.getSuperclass();
+            if (Objects.equals(searchType, Object.class)) {
+                break;
+            }
+        }
+    }
+
+
+    private Method[] getDeclareMethods(Class<?> clazz) {
+        Method[] result;
+        Method[] declaredMethods = clazz.getDeclaredMethods();
+        List<Method> defaultMethods = findConcreteMethodsOnInterfaces(clazz);
+        if (defaultMethods != null) {
+            result = new Method[declaredMethods.length + defaultMethods.size()];
+            System.arraycopy(declaredMethods, 0, result, 0, declaredMethods.length);
+            int index = declaredMethods.length;
+            for (Method defaultMethod : defaultMethods) {
+                result[index] = defaultMethod;
+                index++;
+            }
+        } else {
+            result = declaredMethods;
+        }
+        return result;
+    }
+
+    private List<Method> findConcreteMethodsOnInterfaces(Class<?> clazz) {
+        List<Method> result = null;
+        for (Class<?> ifc : clazz.getInterfaces()) {
+            for (Method ifcMethod : ifc.getMethods()) {
+                if (!Modifier.isAbstract(ifcMethod.getModifiers())) {
+                    if (result == null) {
+                        result = new ArrayList<>();
+                    }
+                    result.add(ifcMethod);
+                }
+            }
+        }
+        return result;
     }
 
 
