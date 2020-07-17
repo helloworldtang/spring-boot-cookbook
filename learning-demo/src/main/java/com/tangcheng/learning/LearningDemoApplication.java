83ae9c00ac5d5fcf31bb942c92f5937c111b8889
唐成 <cheng.tang@gongbangbang.com>
2020-07-17 13:08:06 +0800

    java.lang.ClassNotFoundException: org.springframework.web.server.WebFilter

--- learning-demo/src/main/java/com/tangcheng/learning/LearningDemoApplication.java
+++ learning-demo/src/main/java/com/tangcheng/learning/LearningDemoApplication.java
@@ -1,18 +1,18 @@
 package com.tangcheng.learning;
 
-import com.spring4all.swagger.EnableSwagger2Doc;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 /**
  * Created by tang.cheng on 2017/4/22.
  */
-@EnableSwagger2Doc
+@EnableOpenApi
 @SpringBootApplication
-@Slf4j
-public class DemoApplication {
+public class LearningDemoApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(LearningDemoApplication.class, args);
     }
+
 }
