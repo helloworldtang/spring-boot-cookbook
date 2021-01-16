94ba945937adc3597a9f3dd27ec2633410fb8721
helloworld.tang <helloworld.tang@qq.com>
2021-01-16 18:52:53 +0800

    joda LocalDateTime.parse(dateString, DateTimeFormat.forPattern(

--- learning-demo/src/test/java/com/tangcheng/learning/joda/JodaTest.java
+++ learning-demo/src/test/java/com/tangcheng/learning/joda/JodaTest.java
@@ -2,7 +2,12 @@ package com.tangcheng.learning.joda;
 
 import lombok.extern.slf4j.Slf4j;
 import org.joda.time.*;
+import org.joda.time.format.DateTimeFormat;
 import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -45,6 +50,49 @@ public class JodaTest {
         assertThat(expectedDayNum).isEqualTo(dayNum);
         int expectedHours = Hours.hoursBetween(now, end).getHours();
         assertThat(expectedHours).isEqualTo(dayNum * 24);
+    }
+
+    /**
+     * java.lang.IllegalArgumentException: Invalid format: "2021-01-16 00:00:00" is malformed at " 00:00:00"
+     * at org.joda.time.format.DateTimeFormatter.parseLocalDateTime(DateTimeFormatter.java:900)
+     * at org.joda.time.LocalDateTime.parse(LocalDateTime.java:168)
+     * at org.joda.time.LocalDateTime.parse(LocalDateTime.java:157)
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void given_StringDateTime_when_no_locale_then_IllegalArgumentException() {
+        LocalDateTime.parse("2021-01-16 00:00:00");
+    }
+
+
+    /**
+     * Expecting:
+     * <2021-01-16T00:00:00.000 (java.util.Date)>
+     * to be equal to:
+     * <2021-02-16T00:00:00.922 (java.util.Date)>
+     * but was not.
+     * Expected :2021-02-16T00:00:00.922 (java.util.Date)
+     * Actual   :2021-01-16T00:00:00.000 (java.util.Date)
+     * <p>
+     * 初始值
+     * 代码如下，值得指出的是由于我们的时区设置是GMT+8,所以打印格林威治时间得到的是1970-01-01 08:00:00.
+     * Calendar cal = Calendar.getInstance（）；//得到当前时间
+     * cal.setTimeInMillis（0）；//转换成格林威治时间 1970-01-01T08:00:00.000
+     */
+    @Test
+    public void given_StringDateTime_when_no_locale_then_LocalDateTime() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2021, Calendar.JANUARY, 16, 0, 0, 0);
+        /**
+         * 原因是Calendar.getInstance()时候，会给Calendar赋值为当前时间的毫秒值，调用set方法会根据参数对年、月、日、时、分、秒对应的值进行替换。
+         * 然而无法对毫秒值进行替换，所以毫秒值是创建Calendar对象时的毫秒值。
+         */
+        calendar.set(Calendar.MILLISECOND, 0);
+        Date expected = calendar.getTime();
+        String dateString = "2021-01-16 00:00:00";
+        LocalDateTime localDateTime = LocalDateTime.parse(dateString, DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss").withLocale(Locale.SIMPLIFIED_CHINESE));
+        Date actual = localDateTime.toDate();
+        System.out.println(actual);
+        assertThat(actual).isEqualTo(expected);
     }
 
 
