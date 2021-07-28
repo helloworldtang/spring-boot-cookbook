688e6637bf39028d4e976c48860a7893e04418ed
helloworld.tang <helloworld.tang@qq.com>
2021-07-28 10:34:24 +0800

    swagger

--- app-schedule/src/main/java/com/tangcheng/app/schedule/config/Swagger2Config.java
+++ app-schedule/src/main/java/com/tangcheng/app/schedule/config/Swagger2Config.java
@@ -2,13 +2,18 @@ package com.tangcheng.app.schedule.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by tangcheng on 4/29/2017.
@@ -32,7 +37,14 @@ public class Swagger2Config {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.tangcheng.app.schedule.web"))
                 .build()
-                .apiInfo(apiInfo);
+                .apiInfo(apiInfo)
+                .securitySchemes(securitySchemes());
+    }
+
+    private List<ApiKey> securitySchemes() {
+        List<ApiKey> securitySchemes = new ArrayList<>(1);
+        securitySchemes.add(new ApiKey("Bearer Token", HttpHeaders.AUTHORIZATION, "header"));
+        return securitySchemes;
     }
 
 }
